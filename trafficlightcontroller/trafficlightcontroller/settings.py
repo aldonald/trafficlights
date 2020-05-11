73e60ee47e39bce42ab35f17fa3852fd9a644867
Alex Donald <aldon80@gmail.com>
2020-05-11 15:33:03 +1000

    imported heroku to settings

--- trafficlightcontroller/trafficlightcontroller/settings.py
+++ trafficlightcontroller/trafficlightcontroller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lights'
+    'lights',
 ]
 
 MIDDLEWARE = [
