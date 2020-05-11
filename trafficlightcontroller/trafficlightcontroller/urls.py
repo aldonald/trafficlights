b9797e36690517b226fdb93d5a3d958c9d24d018
Alex Donald <aldon80@gmail.com>
2020-05-11 15:28:49 +1000

    correction to Proc

--- trafficlightcontroller/trafficlightcontroller/urls.py
+++ trafficlightcontroller/trafficlightcontroller/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lights/', include('lights.urls'), name='lights'),
+    path('', include('lights.urls')),
 ]
